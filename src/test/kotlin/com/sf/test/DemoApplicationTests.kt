bb6a7388c2b757b0f9490e92e72e7f91d039b1e3
Michael Czolko <michael@czolko.cz>
2020-11-17 17:11:54 +0100

    Spring init with Kotlin

---
+++ src/test/kotlin/com/sf/test/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sf.test
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
